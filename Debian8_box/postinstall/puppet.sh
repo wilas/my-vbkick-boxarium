d951888e17e8e32a1a51dededea0213e42dd16dd
wilas <limak.kamwil@gmail.com>
2013-09-17 01:13:06 +0200

    update puppet postinstall script

--- Debian8_box/postinstall/puppet.sh
+++ Debian8_box/postinstall/puppet.sh
@@ -1,6 +1,6 @@
-wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb -O /tmp/puppetlabs-release-wheezy.deb
-dpkg -i /tmp/puppetlabs-release-wheezy.deb
-rm -f /tmp/puppetlabs-release-wheezy.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-sid.deb -O /tmp/puppetlabs-release-sid.deb
+dpkg -i /tmp/puppetlabs-release-sid.deb
+rm -f /tmp/puppetlabs-release-sid.deb
 apt-get -y update
 
 apt-get install -y hiera puppet facter
