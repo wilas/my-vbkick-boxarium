fd9ffccee713fe34ad85ab6cedafca6d60720669
wilas <limak.kamwil@gmail.com>
2013-09-22 00:26:31 +0200

    docker from ubuntu to debian Jessy/Sid

---
+++ DebianT_box/validate/test_docker.sh
@@ -0,0 +1,9 @@
+# Feature: lxc-docker
+# Given docker command
+which docker 1>/dev/null 2>&1
+[ $? -ne 0 ] && { printf "\e[1;31mdocker: FAIL\n\e[0m"; exit 1; }
+# When I run "docker version" command
+sudo docker version 1>/dev/null 2>&1
+[ $? -ne 0 ] && { printf "\e[1;31mdocker version: FAIL\n\e[0m"; exit 1; }
+# Then I expect success
+printf "\e[1;32mdocker: OK\n\e[0m";
