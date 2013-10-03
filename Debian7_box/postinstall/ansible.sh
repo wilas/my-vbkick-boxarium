e38341c6218c1bb18e75bce59a2f48f9dbf3901f
wilas <limak.kamwil@gmail.com>
2013-10-04 00:40:33 +0200

    update ansible for Debian boxes to 1.3.2

--- Debian7_box/postinstall/ansible.sh
+++ Debian7_box/postinstall/ansible.sh
@@ -1,7 +1,7 @@
 # Install ansible
 apt-get install -y cdbs debhelper dpkg-dev git-core reprepro python-support python-apt python-paramiko python-yaml python-jinja2 sshpass
 cd /tmp && git clone git://github.com/ansible/ansible.git
-cd /tmp/ansible/ && git checkout release1.3.1 && make deb
+cd /tmp/ansible/ && git checkout release1.3.2 && make deb
 dpkg -i /tmp/ansible*.deb
 rm -rf /tmp/ansible
 rm -f /tmp/ansible*.deb
