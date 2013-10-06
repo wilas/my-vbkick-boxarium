93f678602f89999148bdbf8465d8fe21d80b10a4
wilas <limak.kamwil@gmail.com>
2013-10-06 13:08:29 +0200

    update SL6 postinstall and validate scripts

--- SL6_box/validate/adm_features.sh
+++ SL6_box/validate/adm_features.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+set -eEu
+
 # set environment variables if available
 [[ -s "adm_envrc" ]] && . "./adm_envrc"
 
 # Given a new Virtualbox Guest
-context_file="adm_context.txt"
+if [[ $# -ge 1 ]]; then
+    context_file="${1}"
+else
+    context_file="adm_context.txt"
+fi
 
 # Each script is separate feature
 while read -r script; do
     # don't process comments
     [[ "${script}" =~ ^#.*$ ]] && continue
     if [[ -s "${script}" ]]; then
-        bash "${script}"
+        # true is used because it's ok when tested command fail
+        bash "${script}" || true
     fi
 done < "${context_file}"
