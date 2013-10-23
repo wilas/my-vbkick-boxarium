928fc8bba8c92d15f971aab1b113ff76bfa21c20
wilas <limak.kamwil@gmail.com>
2013-10-24 01:53:55 +0200

    added KDE desktop version

---
+++ Debian7_box/postinstall/desktop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -E -u -o pipefail; shopt -s failglob;
+
+# Add the debian-mozilla repo
+if [[ ! -f "/etc/apt/sources.list.d/mozilla-iceweasel.list" ]]; then
+    echo 'deb http://mozilla.debian.net/ wheezy-backports iceweasel-release' > /etc/apt/sources.list.d/mozilla-iceweasel.list
+    apt-get -y update
+fi
+# Install the new iceweasel from mozilla repo
+apt-get -y install -t wheezy-backports iceweasel
+
+# Add backports (choose your mirror from http://backports.debian.org/Mirrors/)
+#if [[ ! -f "/etc/apt/sources.list.d/wheezy-backports.list" ]]; then
+#    echo 'deb http://ftp.debian.org/debian wheezy-backports main' > /etc/apt/sources.list.d/wheezy-backports.list
+#    apt-get -y update
+#fi
+
+# Flash
+## update repo with contrib and non-free: 'deb http://ftp.uk.debian.org/debian wheezy main contrib non-free'
+#apt-get -y install flashplugin-nonfree
+
+# Java
+# apt-cache search openjdk
+# apt-get -y install openjdk-7-jdk or apt-get -y install openjdk-7-jre
+# apt-get -y install icedtea-7-plugin
