5f93ca78b0d25f2fba1e60bec76c98d3289dde6c
wilas <limak.kamwil@gmail.com>
2013-10-27 23:17:11 +0100

    added KDE desktop version with polish keyaboard for debian testing

---
+++ DebianTU_box/postinstall/desktop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -E -u -o pipefail; shopt -s failglob;
+
+# Add the cdn-unstable repo (more: http://mozilla.debian.net/)(using this you mix distribution, read: https://wiki.debian.org/AptPreferences)
+#if [[ ! -f "/etc/apt/sources.list.d/cdn-unstable.list" ]]; then
+#    echo 'deb http://cdn.debian.net/debian unstable main' > /etc/apt/sources.list.d/cdn-unstable.list
+#    apt-get -y update
+#fi
+# Install the new iceweasel from unstable repo
+#apt-get -y install -t unstable iceweasel
+
+# Add backports (choose your mirror from http://backports.debian.org/Mirrors/)(done in preseed.cfg)
+#if [[ ! -f "/etc/apt/sources.list.d/jessie-backports.list" ]]; then
+#    echo 'deb http://ftp.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/jessie-backports.list
+#    apt-get -y update
+#fi
+
+# Flash
+## update repo with contrib and non-free (done in preseed.cfg): 'deb http://ftp.uk.debian.org/debian jessie main contrib non-free'
+#apt-get -y install flashplugin-nonfree
+
+# Java
+# apt-cache search openjdk
+# apt-get -y install openjdk-7-jdk or apt-get -y install openjdk-7-jre
+# apt-get -y install icedtea-7-plugin
