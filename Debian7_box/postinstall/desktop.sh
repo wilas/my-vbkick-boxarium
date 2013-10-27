46dee6f746365af492be6ca9b061ad4cce7655f4
wilas <limak.kamwil@gmail.com>
2013-10-27 14:15:24 +0100

    added mozilla keyring to desktop postinstall script

--- Debian7_box/postinstall/desktop.sh
+++ Debian7_box/postinstall/desktop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
-# Add the debian-mozilla repo
+# Add the debian-mozilla repo (http://mozilla.debian.net/)
 if [[ ! -f "/etc/apt/sources.list.d/mozilla-iceweasel.list" ]]; then
     echo 'deb http://mozilla.debian.net/ wheezy-backports iceweasel-release' > /etc/apt/sources.list.d/mozilla-iceweasel.list
+    wget -O /tmp/mozilla-keyring.deb --no-check-certificate http://mozilla.debian.net/pkg-mozilla-archive-keyring_1.1_all.deb
+    dpkg -i /tmp/mozilla-keyring.deb
+    rm -f /tmp/mozilla-keyring.deb
     apt-get -y update
 fi
 # Install the new iceweasel from mozilla repo
@@ -16,7 +19,7 @@ apt-get -y install -t wheezy-backports iceweasel
 #fi
 
 # Flash
-## update repo with contrib and non-free (may be done in preseed.cfg): 'deb http://ftp.uk.debian.org/debian wheezy main contrib non-free'
+## update repo with contrib and non-free (done in preseed.cfg): 'deb http://ftp.uk.debian.org/debian wheezy main contrib non-free'
 #apt-get -y install flashplugin-nonfree
 
 # Java
