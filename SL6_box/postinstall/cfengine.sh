465d59e22a64d5c4e2ea04a51d94d5f33f110905
wilas <limak.kamwil@gmail.com>
2013-12-02 23:53:24 +0100

    update SL6 posintsall scripts

--- SL6_box/postinstall/cfengine.sh
+++ SL6_box/postinstall/cfengine.sh
@@ -2,9 +2,8 @@
 set -e -E -u -o pipefail; shopt -s failglob;
 
 if [[ ! -f "/etc/yum.repos.d/cfengine-community.repo" ]]; then
-    wget -O /tmp/gpg_cfengine.key http://cfengine.com/pub/gpg.key
-    rpm --import /tmp/gpg_cfengine.key
-    rm -f /tmp/gpg_cfengine.key
+    rpm --import http://cfengine.com/pub/gpg.key
+    # to check: rpm -qi gpg-pubkey | grep -i cfengine
     printf "[cfengine-repository]\nname=CFEngine\nbaseurl=http://cfengine.com/pub/yum/\nenabled=1\ngpgcheck=1\n" > /etc/yum.repos.d/cfengine-community.repo
 fi
 yum -y --enablerepo=cfengine-repository install cfengine-community
