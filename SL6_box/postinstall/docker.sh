92a35fa3f5c8118f539c4a2d2df924d55bc66364
wilas <limak.kamwil@gmail.com>
2013-12-06 22:57:06 +0100

    update docker postinstall script for SL6

--- SL6_box/postinstall/docker.sh
+++ SL6_box/postinstall/docker.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
-printf "[WARNING] BETA script - lxc not yet in epel.\n"
-
 # Install docker-io
-yum -y --enablerepo=epel-testing install docker-io
+yum -y --enablerepo=epel install docker-io
 
 if ! service docker status > /dev/null; then
     service docker restart
     service docker status || true
-    # Usually: 'docker dead but subsys locked'
-    # Better is just reboot host.
-    # Note: docker service is by default enabled to start automatically during the system bootup
+    # Usually: 'docker dead but subsys locked', better is just reboot host.
 fi
-
-# Inspection:
-# chkconfig --list docker
-# chkconfig --list cgconfig
-# sysctl net.ipv4.ip_forward
-# ls /cgroup/
-# less /etc/init.d/docker
-# less /etc/cgconfig.conf
-# less /etc/sysconfig/selinux
-# less /etc/fstab
-# less /etc/grub.conf
 
 # Quick test
 # sudo lxc-checkconfig
@@ -31,5 +16,3 @@ fi
 # sudo docker info
 # sudo docker run -i -t ubuntu /bin/bash
 # sudo docker run -dns 8.8.8.8 centos ping google.com
-
-# Debug pulling: curl -v https://registry-1.docker.io/_ping
