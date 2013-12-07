d7065a9a0055de30897f391559af18f8b77932cb
wilas <limak.kamwil@gmail.com>
2013-12-07 20:51:02 +0100

    update docker scripts to v0.7

--- Debian7_box/postinstall/kernel_backports.sh
+++ Debian7_box/postinstall/kernel_backports.sh
@@ -2,7 +2,6 @@
 set -e -E -u -o pipefail; shopt -s failglob;
 
 # Debian backports: http://backports.debian.org/Instructions/
-# Add backports (choose your mirror from http://backports.debian.org/Mirrors/)
 if [[ ! -f "/etc/apt/sources.list.d/wheezy-backports.list" ]]; then
     echo 'deb http://ftp.debian.org/debian wheezy-backports main' > /etc/apt/sources.list.d/wheezy-backports.list
     apt-get -y update
@@ -10,7 +9,6 @@ fi
 
 # Install the new linux kernel and headers from backports
 apt-get -y install -t wheezy-backports linux-image-amd64 linux-headers-amd64
-#kversion="3.10-0.bpo.2" && apt-get -y install -t wheezy-backports linux-image-$kversion-amd64 linux-headers-$kversion-amd64 
 
-# Note: AUFS is included in the kernels built by the Debian and Ubuntu distributions, is not part of the standard kernel. 
-# More: http://docs.docker.io/en/latest/installation/kernel/#kernel
+# Note1: AUFS is included in the kernels built by the Debian and Ubuntu distributions, 
+# is not part of the standard kernel, more: http://docs.docker.io/en/latest/installation/kernel/#kernel
--- Debian7_box/postinstall/virtualbox.sh
+++ Debian7_box/postinstall/virtualbox.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
+VBOX_VERSION=${VBOX_VERSION:-"0.0.0"}
+# Do we have any virtualbox guest additions installed ?
+if VBoxControl --version >/dev/null 2>&1; then
+    version=$(VBoxControl --version | cut -f 1 -d"r") # 4.2.12r84980 -> 4.2.12
+    # Expected version is installed, exit
+    if [[ $version == "${VBOX_VERSION}" ]]; then
+        exit 0
+    fi
+fi
+
 # Installing the virtualbox guest additions
-# need kernel headers to compile module and virtualbox tools
-apt-get -y install linux-headers-$(uname -r) dkms
-# apt-get -y install libdbus-1-3
-# VBoxGuestAdditions iso is attached (by vbkick) to IDE Controller port 1 device 0
+# VBoxGuestAdditions iso is attached (by vbkick) to SATA Controller port 1 device 0
 if [[ -b /dev/sr1 ]]; then
     mount /dev/sr1 /mnt
 elif [[ -b /dev/sr0 ]]; then
