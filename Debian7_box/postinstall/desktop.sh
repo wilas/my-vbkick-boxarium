36c8023b665080d3a05e9c7365b950066b684b65
wilas <limak.kamwil@gmail.com>
2014-05-19 00:44:05 +0200

    update debian7 postinstall scripts

--- Debian7_box/postinstall/desktop.sh
+++ Debian7_box/postinstall/desktop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
-# Add the debian-mozilla repo (http://mozilla.debian.net/)
+# The new iceweasel
+## Add the debian-mozilla repo (http://mozilla.debian.net/)
 if [[ ! -f "/etc/apt/sources.list.d/mozilla-iceweasel.list" ]]; then
     echo 'deb http://mozilla.debian.net/ wheezy-backports iceweasel-release' > /etc/apt/sources.list.d/mozilla-iceweasel.list
     wget -O /tmp/mozilla-keyring.deb --no-check-certificate http://mozilla.debian.net/pkg-mozilla-archive-keyring_1.1_all.deb
@@ -9,14 +10,8 @@ if [[ ! -f "/etc/apt/sources.list.d/mozilla-iceweasel.list" ]]; then
     rm -f /tmp/mozilla-keyring.deb
     apt-get -y update
 fi
-# Install the new iceweasel from mozilla repo
+## Install the new iceweasel from mozilla repo
 apt-get -y install -t wheezy-backports iceweasel
-
-# Add backports (choose your mirror from http://backports.debian.org/Mirrors/)
-#if [[ ! -f "/etc/apt/sources.list.d/wheezy-backports.list" ]]; then
-#    echo 'deb http://ftp.debian.org/debian wheezy-backports main' > /etc/apt/sources.list.d/wheezy-backports.list
-#    apt-get -y update
-#fi
 
 # Flash
 ## update repo with contrib and non-free (done in preseed.cfg): 'deb http://ftp.uk.debian.org/debian wheezy main contrib non-free'
--- Debian7_box/postinstall/kernel_backports.sh
+++ Debian7_box/postinstall/kernel_backports.sh
@@ -10,5 +10,5 @@ fi
 # Install the new linux kernel and headers from backports
 apt-get -y install -t wheezy-backports linux-image-amd64 linux-headers-amd64
 
-# Note1: AUFS is included in the kernels built by the Debian and Ubuntu distributions, 
-# is not part of the standard kernel, more: http://docs.docker.io/en/latest/installation/kernel/#kernel
+# NB1: AUFS is included in the kernels built by the Debian and Ubuntu distributions,
+# is not a part of the standard kernel, more: http://docs.docker.io/en/latest/installation/kernel/#kernel
