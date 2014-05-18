11af034f0465829baebd79b4176ceda5d6e6dcc9
wilas <limak.kamwil@gmail.com>
2014-05-19 00:41:45 +0200

    update debian7 ansible postinstall script to v1.6.1

--- Debian7_box/postinstall/ansible.sh
+++ Debian7_box/postinstall/ansible.sh
@@ -2,9 +2,10 @@
 set -e -E -u -o pipefail; shopt -s failglob;
 
 # Install ansible
-version="1.4.1"
+version="1.6.1"
 if ! dpkg -l ansible | grep -qw "${version}"; then
-    apt-get install -y cdbs debhelper dpkg-dev git reprepro python-support python-apt python-paramiko python-yaml python-jinja2 sshpass
+    apt-get install -y cdbs debhelper dpkg-dev git reprepro python-apt python-support fakeroot asciidoc python-setuptools
+    apt-get install -y python-paramiko python-yaml python-jinja2 python-httplib2 sshpass #python-keyczar
     cd /tmp && git clone git://github.com/ansible/ansible.git
     cd /tmp/ansible/ && git checkout "release${version}" && make deb
     dpkg -i /tmp/ansible*.deb
