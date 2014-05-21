32989e912ebf336c3ba3cf8f572745f98e96c14b
wilas <limak.kamwil@gmail.com>
2014-05-22 00:02:15 +0200

    mv play to common in SL6

--- DebianSid_box/postinstall/puppet.sh
+++ DebianSid_box/postinstall/puppet.sh
@@ -2,7 +2,7 @@
 set -e -E -u -o pipefail; shopt -s failglob;
 
 if [[ ! -f "/etc/apt/sources.list.d/puppetlabs.list" ]]; then
-    wget -O /tmp/puppetlabs-release-sid.deb http://apt.puppetlabs.com/puppetlabs-release-sid.deb 
+    wget -O /tmp/puppetlabs-release-sid.deb http://apt.puppetlabs.com/puppetlabs-release-sid.deb
     dpkg -i /tmp/puppetlabs-release-sid.deb
     rm -f /tmp/puppetlabs-release-sid.deb
     apt-get -y update
--- DebianTU_box/postinstall/chef.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e -E -u -o pipefail; shopt -s failglob;
-
-# Installing chef
-gem install --no-ri --no-rdoc chef
-#curl -Lk https://www.opscode.com/chef/install.sh | bash
