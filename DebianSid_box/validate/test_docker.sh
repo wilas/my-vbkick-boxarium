d3c88f289f645cec8854e54987236761b7cb5d53
wilas <limak.kamwil@gmail.com>
2014-05-24 17:01:23 +0200

    better docker validation - be more specific

--- DebianSid_box/validate/test_docker.sh
+++ DebianSid_box/validate/test_docker.sh
@@ -3,12 +3,12 @@ set -e -E -u -o pipefail; shopt -s failglob;
 
 # Feature: docker.io
 # Given docker command
-if ! command -v docker >/dev/null 2>&1 && ! command -v docker.io >/dev/null 2>&1; then
+if ! command -v docker.io >/dev/null 2>&1; then
     printf "\e[1;31mdocker: FAIL\n\e[0m"
     exit
 fi
 # When I run "docker version" command
-if ! sudo docker version >/dev/null 2>&1 && ! sudo docker.io version >/dev/null 2>&1; then
+if ! sudo docker.io version >/dev/null 2>&1; then
     printf "\e[1;31mdocker version: FAIL\n\e[0m"
     exit
 fi
--- SL6_box/validate/test_docker.sh
+++ SL6_box/validate/test_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e -E -u -o pipefail; shopt -s failglob;
 
-# Feature: lxc-docker
+# Feature: docker.io
 # Given docker command
-if ! command -v docker >/dev/null 2>&1 && ! command -v lxc-docker >/dev/null 2>&1; then
+if ! command -v docker >/dev/null 2>&1; then
     printf "\e[1;31mdocker: FAIL\n\e[0m"
     exit
 fi
 # When I run "docker version" command
-if ! sudo docker version >/dev/null 2>&1 && ! sudo lxc-docker version >/dev/null 2>&1; then
+if ! sudo docker version >/dev/null 2>&1; then
     printf "\e[1;31mdocker version: FAIL\n\e[0m"
     exit
 fi
